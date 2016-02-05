0c358283e137a41a81ed8c3efc491a8e992aa72e
smartsystemsinc <cory@smartsystemsaz.com>
2016-02-05 13:57:20 -0600

    Updated installer to include Email::Sender::Simple
    Also cleaned it up slightly as per perlcritic(1p)

--- sPerlCPAN.pl
+++ sPerlCPAN.pl
@@ -2,15 +2,20 @@
 
 use strict;
 use warnings;
+use English qw(-no_match_vars);
 
-system("cpan App::cpanminus");
-system("cpan Algorithm::Loops");
-system("cpanm Config::Simple");
-system("cpanm Date::Manip");
-system("cpanm Excel::Writer::XLSX");
-system("cpanm Net::FTP");
-system("cpanm --notest PerlIO::Util");
-system("cpanm Smart::Comments");
-if ($^O eq "MSWin32") {
-    system("cpanm Win32::Autoglob");
+our $VERSION = '0.5';
+
+system 'cpan App::cpanminus';
+system 'cpan Algorithm::Loops';
+system 'cpanm Config::Simple';
+system 'cpanm Date::Manip';
+system 'cpanm Excel::Writer::XLSX';
+system 'cpanm Net::FTP';
+system 'cpanm --notest PerlIO::Util';
+system 'cpanm Smart::Comments';
+system 'cpanm Email::Sender::Simple';
+
+if ( $OSNAME eq 'MSWin32' ) {
+    system 'cpanm Win32::Autoglob';
 }
